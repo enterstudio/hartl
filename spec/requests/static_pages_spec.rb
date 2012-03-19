599d4b774b338a3a66a421b6276e5532f0f97e06
Sambit <34sambit@gmail.com>
2012-03-18 23:32:03 -0500

    makes some progress in failing tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,47 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Botflip | Home'" do
+    it "should have the content 'Welcome to Botflip!'" do
       visit '/static_pages/home'
-      page.should have_content('Botflip | Home')
+      page.should have_selector('h1', 
+                                :text => "Welcome to Botflip!")
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => 'Botflip | Home')
     end
   end
+  
+  describe "Help page" do
+
+      it "should have the content 'Help'" do
+        visit '/static_pages/help'
+        page.should have_selector('h1', 
+                                  :text => 'Help')
+      end
+      
+      it "should have the right title" do
+        visit '/static_pages/help'
+        page.should have_selector('title',
+                                  :text => 'Botflip | Help')
+      end
+    end
+    
+  describe "About page" do
+
+      it "should have the content 'About Us'" do
+        visit '/static_pages/about'
+        page.should have_selector('h1', 
+                                  :text => 'About Us')
+      end
+      
+      it "should have the right title" do
+        visit '/static_pages/about'
+        page.should have_selector('title',
+                                  :text => 'Botflip | About Us')
+      end
+    end  
+    
 end
