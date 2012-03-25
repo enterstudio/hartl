52af4124fa251e4e6b273ba12f38097827467261
Sambit <34sambit@gmail.com>
2012-03-24 20:42:36 -0500

    add glyphicons to interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Botflip::Application.routes.draw do
   get "users/new"
 
   root to: 'static_pages#home'
-  
+
   resources :users
-  
-  match '/help', to: 'static_pages#help'
-  match '/about', to: 'static_pages#about'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
   match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
