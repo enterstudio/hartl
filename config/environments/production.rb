3f64bc768a89ed4fb5ec018105fb9c3b62d4d6bb
Sambit <34sambit@gmail.com>
2012-05-27 20:35:39 -0700

    Expires header set in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Botflip::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  
+  # Expires header
+  config.static_cache_control = "public, max-age=3600"
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -9,7 +12,7 @@ Botflip::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
