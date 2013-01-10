e57fbbff45ae2f471cf3194360b039401d0870c1
34code <34sambit@gmail.com>
2013-01-09 16:32:43 -0800

    starting from scratch again

--- app/models/micropost.rb
+++
@@ -1,8 +0,0 @@
-class Micropost < ActiveRecord::Base
-  belongs_to :user
-  attr_accessible :content
-  validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 280 }
-  
-  default_scope order: 'microposts.created_at DESC'
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Botflip::Application.config.secret_token = 'a14f999d8f6773a62b42d1bc70df5754973ecc73d3edcb20a57409c43ad0ff1dd35c0de1d099dc08b5aaac01014f650fb17c618cc43789296d0c797979358be2'
